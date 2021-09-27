80bf1b15432b0e64e80c4f0a8d5f85c7f5969e9e
Grever Fernández <fernandez.grever@gmail.com>
2021-09-26 22:00:40 -0500

    v0.3
    Modo AP y STA.
    Login para WiFI.
    Dashboard bonito y funcional.

--- WebServer_ControlPiscina.ino
+++ WebServer_ControlPiscina.ino
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <EEPROM.h>
 #include <SPIFFS.h>
 #include <ESPAsyncWebServer.h>
 #include <WebSocketsServer.h>
@@ -8,14 +9,23 @@
 //const char  *ssid = "PoolControlSystem";
 //const char  *password =  "12345678";
 
-const char  *ssid = "TopGun";
-const char  *password = "Maveric-k";
+const char  *ssid = "";
+const char  *password = "";
+
+const char  *PARAM_INPUT_1 = "input1";
+const char  *PARAM_INPUT_2 = "input2";
+
+
+const char  *soft_ap_ssid = "SistemaControlPiscina";
+const char  *soft_ap_password = "12345678";
 
 const char  *msg_toggle_led = "toggleLED";
 const char  *msg_get_led = "getLEDState";
 const int   http_port = 80;
 const int   ws_port = 1337;
 const int   led_pin = 2;
+
+int ip;
 
 // Globals
 AsyncWebServer server(http_port);
@@ -88,10 +98,7 @@ void onWebSocketEvent(uint8_t client_num,
 
 // Callback: send homepage
 void onIndexRequest(AsyncWebServerRequest *request) {
-  IPAddress remote_ip = request->client()->remoteIP();
-  Serial.println("[" + remote_ip.toString() +
-                 "] HTTP GET request of " + request->url());
-  request->send(SPIFFS, "/index.html", "text/html");
+
 }
 
 // Callback: send style sheet
@@ -110,6 +117,52 @@ void onPageNotFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
+String readIP() {
+  if (WiFi.status() == WL_CONNECTED) {
+    
+    return (WiFi.localIP().toString());
+  }
+  return String("DESCONECTADO");
+}
+
+void initSTA() {
+  if ((ssid != "") && (password != "")) {
+    int tryCount;
+
+    WiFi.begin(ssid, password);
+
+    Serial.println("");
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      tryCount++;
+      delay(500);
+      if (tryCount > 5) {
+        ssid = "";
+        password = "";
+        Serial.println("ERROR TRYING TO CONNECTING");
+        break;
+      }
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("");
+      Serial.println("WiFi connected.");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+
+      //WiFi.mode(WIFI_STA);
+    }
+  }
+}
+
+String processor(const String & var) {
+  //Serial.println(var);
+  if (var == "IP") {
+    return readIP();
+  }
+}
 
 void setup() {
   // Init LED and turn off
@@ -119,35 +172,74 @@ void setup() {
   // Start Serial port
   Serial.begin(115200);
 
+  // Start ESP32 Wifi Mode as Access Point (AP) and Station Mode (STA)
+  WiFi.mode(WIFI_MODE_APSTA);
+
   // Make sure we can read the file system
   if ( !SPIFFS.begin()) {
     Serial.println("Error mounting SPIFFS");
     while (1);
   }
 
-  //-----------------------------------------------------
-  WiFi.begin(ssid, password);
+  // Start the WiFi STA Mode
+  //  If we have the ssid and password we try to connect
+  initSTA();
+  // End the WiFi STA Mode
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+  // Start the WiFi AP Mode
+  WiFi.softAP(soft_ap_ssid, soft_ap_password);
 
   Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.print("ESP32 IP as soft AP: ");
+  Serial.println(WiFi.softAPIP());
+  // End the WiFi AP Mode
 
-  //-----------------------------------------------------
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    if (ON_AP_FILTER(request)) {
+      // On HTTP request for root, provide index.html file
+      Serial.println("sending");
+      request->send(SPIFFS, "/login_index.html", "text/html");
+    } else {
+      IPAddress remote_ip = request->client()->remoteIP();
+      Serial.println("[" + remote_ip.toString() +
+                     "] HTTP GET request of " + request->url());
+      request->send(SPIFFS, "/index.html", "text/html");
+      WiFi.mode(WIFI_STA);
+    }
+  });
+
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    if (ON_AP_FILTER(request)) {
+      String wifi;
+      String pass;
+
+      // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+      if (request->hasParam(PARAM_INPUT_1)) {
+        wifi = request->getParam(PARAM_INPUT_1)->value();
+        ssid = wifi.c_str();    // c_str() -> Convert String to const char*
+        Serial.println(ssid);
+        pass = request->getParam(PARAM_INPUT_2)->value();
+        password = pass.c_str();
+        Serial.println(password);
+        request->send(SPIFFS, "/login_index.html", "text/html");
+        initSTA();
+      }
+    }
+  });
+
+  server.on("/ip", HTTP_GET, [](AsyncWebServerRequest * request) {
+    if (ON_AP_FILTER(request)) {
+      Serial.println("THERE U GO BOY");
+      request->send(200, "text/plain", readIP().c_str());
+    }
+  });
 
   // On HTTP request for root, provide index.html file
-  server.on("/", HTTP_GET, onIndexRequest);
-
-  // On HTTP request for style sheet, provide style.css
-  server.on("/style.css", HTTP_GET, onCSSRequest);
+  //server.on("/", HTTP_GET, );
 
   // Handle requests for pages that do not exist
-  server.onNotFound(onPageNotFound);
+  //server.onNotFound(onPageNotFound);
 
   // Start web server
   server.begin();
@@ -161,5 +253,4 @@ void setup() {
 void loop() {
   // Look for and handle WebSocket data
   webSocket.loop();
-
 }
